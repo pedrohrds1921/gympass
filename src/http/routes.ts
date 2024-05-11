cd827995ce240eebe1ae36ec2b99174900c83ecf
Pedro Rodrigues <pedrohrds@Pedros-iMac-Pro.local>
2024-05-11 17:37:13 -0300

    Add get route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
 import { registerController } from './controllers/register-controller'
 
 export async function appRoutes(app: FastifyInstance) {
+  app.get('/', (request: FastifyRequest, reply: FastifyReply) => {
+    reply.send({ message: 'Te amo meu amor <3' })
+  })
   app.post('/users', registerController)
 }
